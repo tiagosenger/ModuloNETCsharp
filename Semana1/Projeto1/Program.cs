c00a72419e94f3a282efcacdbc0c11adc7460fef
Tiago Senger <tiagosengerds@gmail.com>
2023-11-09 17:18:14 -0300

    finalização aula

--- Semana1/Projeto1/Program.cs
+++ Semana1/Projeto1/Program.cs
@@ -7,8 +7,25 @@
     string tipoString;
     long tipoLong;
 
+   // tipoInteiro = tipoLong;
     tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+    
+    tipoString = "100";
+
+    tipoInteiro = int.Parse(tipoString);
 
     Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
-#endregion 
+    Console.WriteLine("O máximo long é: " + tipoLong);
 
+#endregion
+
+#region Operadores
+
+    tipoDouble = tipoInteiro + tipoLong;
+    //tipoInteiro = tipoDouble + tipoLong; erro, nem long nem double podem ser convertidos a um inteiro
+    //tipoLong = tipoDouble + tipoInteiro; erro, um double n pode ser convertido num long (slide 9 aula 2)
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+
+#endregion
