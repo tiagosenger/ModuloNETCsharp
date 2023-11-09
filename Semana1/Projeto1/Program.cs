333193cc44ef60fe092b3f2723d7be93d7b8d8ec
Tiago Senger <tiagosengerds@gmail.com>
2023-11-09 16:49:39 -0300

    Primeiro exercicio

--- Semana1/Projeto1/Program.cs
+++ Semana1/Projeto1/Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Olá, Mundo!");
+
+#region Teste de Tipos de Dados
+   
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+
+    Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
+#endregion 
+
