7b267b09a2cde7ac55bfa53b7f5bae59f5f0f8b9
Tiago Senger <tiagosengerds@gmail.com>
2023-11-16 15:36:34 -0300

    intervalo

--- Semana2/Projeto2/Program.cs
+++ Semana2/Projeto2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main()
+    {
+        Console.WriteLine("Números divísiveis por 3 ou 4 entre 0 e 30:");
+
+        for (int i = 0; i <= 30; i++){
+            if (i % 3 == 0  || i % 4 == 0)
+            {
+                Console.WriteLine(i);
+            }{}
+        }
+
+        Console.WriteLine("\nPrimeiros números da série de Fibonacci:");
+
+        int limite = 100;
+        int a = 0, b = 1;
+
+        Console.Write($"{a} {b} ");
+
+        while (a + b <= limite)
+        {
+            int proximo = a + b;
+            Console.Write($"{proximo} ");
+            a = b;
+            b = proximo;
+        }
+        
+        Console.Write("\n");
+
+    }
+}
