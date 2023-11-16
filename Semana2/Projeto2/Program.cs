be2a61ec8c2c392d129bc021f4ca90cd88907260
Tiago Senger <tiagosengerds@gmail.com>
2023-11-16 16:59:16 -0300

    correções

--- Semana2/Projeto2/Program.cs
+++ Semana2/Projeto2/Program.cs
@@ -1,46 +1,42 @@
-﻿using System;
+﻿#region Exercicios aula
 
-class Program {
-    static void Main()
+Console.WriteLine("Números divísiveis por 3 ou 4 entre 0 e 30:");
+
+for (int i = 0; i <= 30; i++){
+    if (i % 3 == 0  || i % 4 == 0)
     {
-        Console.WriteLine("Números divísiveis por 3 ou 4 entre 0 e 30:");
-
-        for (int i = 0; i <= 30; i++){
-            if (i % 3 == 0  || i % 4 == 0)
-            {
-                Console.WriteLine(i);
-            }{}
-        }
-
-        Console.WriteLine("\nPrimeiros números da série de Fibonacci:");
-
-        int limite = 100;
-        int a = 0, b = 1;
-
-        Console.Write($"{a} \n{b} \n");
-
-        while (a + b <= limite)
-        {
-            int proximo = a + b;
-            Console.Write($"{proximo} \n");
-            a = b;
-            b = proximo;
-        }
-        
-        Console.Write("\nTabela: \n");
-
-        int nivelMaximo = 8;
-
-        for (int n = 1; n <= nivelMaximo; n++)
-        {
-            Console.Write($"n = {n}: ");
-
-            for (int i = 1; i <= n; i++)
-            {
-                Console.Write($"{n * i} ");
-            }
-
-            Console.WriteLine();
-        }     
-    }
+        Console.WriteLine(i);
+    }{}
 }
+
+Console.WriteLine("\nPrimeiros números da série de Fibonacci:");
+
+int limite = 100;
+int a = 0, b = 1;
+
+Console.Write($"{a} \n{b} \n");
+
+while (a + b <= limite)
+{
+    int proximo = a + b;
+    Console.Write($"{proximo} \n");
+    a = b;
+    b = proximo;
+}
+
+Console.Write("\nTabela: \n");
+
+int nivelMaximo = 8;
+
+for (int n = 1; n <= nivelMaximo; n++)
+{
+    Console.Write($"n = {n}: ");
+
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{n * i} ");
+    }
+
+    Console.WriteLine();
+}
+#endregion
