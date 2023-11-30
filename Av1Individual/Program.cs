2149cb11699f8bc0a23341a8fd7430e9f7b5ce74
Tiago Senger <tiagosengerds@gmail.com>
2023-11-30 15:46:18 -0300

    Primeiro commit

---
+++ Av1Individual/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Sistema de Gerenciamento de Escritórios de Advocacia
+
+class Advogado{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+    public string CNA { get; set; }
+}
+
+class Cliente{
+    public string Nome { get; set;}
+    public DateTime DataNascimento {get; set; }
+    public string CPF { get; set; }
+    public string EstadoCivil { get; set; }
+    public string Profissao {get; set;}
+}
+
+
+class EscritorioAdvocacia{
+
+}
+
+#endregion
