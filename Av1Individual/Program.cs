41f8c6c09bb5548cc300e68cdec43d95f249a4b7
Tiago Senger <tiagosengerds@gmail.com>
2023-11-30 16:00:25 -0300

    Quarto Commit

--- Av1Individual/Program.cs
+++ Av1Individual/Program.cs
@@ -1,36 +1,39 @@
 ﻿#region Sistema de Gerenciamento de Escritórios de Advocacia
 
 class Advogado{
-    public string Nome { get; set; }
+    public string Nome { get; set; } = string.Empty;
     public DateTime DataNascimento { get; set; }
-    public string CPF { get; set; }
-    public string CNA { get; set; }
+    public string CPF { get; set; } = string.Empty;
+    public string CNA { get; set; } = string.Empty;
 }
 
 class Cliente{
-    public string Nome { get; set;}
+    public string Nome { get; set;} = string.Empty;
     public DateTime DataNascimento {get; set; }
-    public string CPF { get; set; }
-    public string EstadoCivil { get; set; }
-    public string Profissao {get; set;}
+    public string CPF { get; set; } = string.Empty;
+    public string EstadoCivil { get; set; } = string.Empty;
+    public string Profissao {get; set;} = string.Empty;
 }
-
 
 class EscritorioAdvocacia{
     private List<Advogado> advogados = new List<Advogado>();
     private List<Cliente> clientes = new List<Cliente>();
-
+// Primeiro Requisito - Coleção de Advogados com validação de CPF e CNA:
     public void AdicionarAdvogado(Advogado advogado){
         if (advogados.Any(a => a.CPF == advogado.CPF || a.CNA == advogado.CNA)){
             throw new ArgumentException("CPF ou CNA já cadastrados.");
         }
-
         advogados.Add(advogado);
     }
-
+// Segundo Requisito - Coleção de Clientes com validação de CPF:
     public void AdicionarCliente(Cliente cliente){
-        
+        if (clientes.Any(c => c.CPF == cliente.CPF)){
+            throw new ArgumentException("CPF ja cadastrado.");
+        }
+        clientes.Add(cliente);    
     }
+// Métodos dos Relatórios Solicitados:
+    public 
 }
 
 #endregion
